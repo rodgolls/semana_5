6be28003595400e17f89292f7cbaefdcd069ab5d
rodgolls <rodgolls@hotmail.com>
2020-03-31 16:26:59 -0300

    'entrega5certo'

---
+++ Pares.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+public class Pares{
+   public static void main (String ... args){
+      for(int a = 0; a <= 200; a = a + 2){
+         JOptionPane.showMessageDialog(null,a);
+      }
+   } 
+}
